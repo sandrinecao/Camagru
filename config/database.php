0e18b95fa171885e4e2f914f355b56c039d37a2d
Sandrine CAO <scao@e3r3p8.42.fr>
2019-12-04 17:42:18 +0100

    further changes

--- config/database.php
+++ config/database.php
@@ -3,11 +3,11 @@
 $DB_DSN = 'mysql:host=127.0.0.1:3306; dbname=db_camagru';
 $DB_USER = 'root';
 $DB_PASSWORD = "elpintor";
- 
-try{
+
+try {
     $pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e){
+} catch (PDOException $e) {
     die("ERROR: Could not connect. " . $e->getMessage());
 }
 ?>
